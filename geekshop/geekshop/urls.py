8262390d218ba32f8ea36844341d2dc3abcfe71a
alekseevalvl <alekseev-alvl@yandex.ru>
2022-01-20 17:24:12 +0300

    Second home work

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -13,13 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from os import name
 from django.contrib import admin
 from django.urls import path
 from mainapp import views as mainapp
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', mainapp.index),
-    path('contact/', mainapp.contact),
-    path('products/', mainapp.products),
+    path('', mainapp.index, name='main'),
+    path('contact/', mainapp.contact, name='contact'),
+    path('products/', mainapp.products, name='products'),
 ]
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 
-# Create your views here.
+MENU_LINKS = [
+    {'url':'main', 'name':'Домой'},
+    {'url':'products', 'name':'Продукты'},
+    {'url':'contact', 'name':'Контакты'},
+]
 def index(request):
-    return render(request, 'mainapp/index.html')
+    return render(request, 'mainapp/index.html', context={
+        'title':'Главная',
+        'menu_links': MENU_LINKS, 
+    })
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    return render(request, 'mainapp/contact.html', context={
+        'title':'Контакты',
+        'menu_links': MENU_LINKS, 
+    })
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    return render(request, 'mainapp/products.html', context={
+        'title':'Продукты',
+        'menu_links': MENU_LINKS, 
+    })

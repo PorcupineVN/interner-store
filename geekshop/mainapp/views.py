066687b1fd91a0a978277e74a26accb551ef06f0
alekseevalvl <alekseev-alvl@yandex.ru>
2022-01-24 17:51:42 +0300

    homework 3

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,14 +1,18 @@
+from unicodedata import category
 from django.shortcuts import render
+from .models import Product, ProductCategory
 
 MENU_LINKS = [
     {'url':'main', 'name':'Домой'},
-    {'url':'products', 'name':'Продукты'},
+    {'url':'products:products', 'name':'Продукты'},
     {'url':'contact', 'name':'Контакты'},
 ]
 def index(request):
+    products = Product.objects.all()[:4]
     return render(request, 'mainapp/index.html', context={
         'title':'Главная',
-        'menu_links': MENU_LINKS, 
+        'menu_links': MENU_LINKS,
+        'products': products, 
     })
 
 def contact(request):
@@ -18,7 +22,17 @@ def contact(request):
     })
 
 def products(request):
-    return render(request, 'mainapp/products.html', context={
+    categories = ProductCategory.objects.all()
+    return render(
+        request, 
+        'mainapp/products.html', 
+        context={
         'title':'Продукты',
-        'menu_links': MENU_LINKS, 
-    })
+        'products':[],
+        'menu_links': MENU_LINKS,
+        'categories': categories, 
+        },
+    )
+
+def category(request, pk):
+    return products(request)
